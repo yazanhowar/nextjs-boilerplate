27548b143bb75fbb4a85a315149ec5211a259546
yazanhowar <yazanhowar@gmail.com>
2026-06-17 23:37:01 +0300

    fix: disable no-img-element eslint rule to allow <img> in BankLogo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,15 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+...nextVitals,
+...nextTs,
+{ rules: { '@next/next/no-img-element': 'off' } },
+globalIgnores([
+".next/**",
+"out/**",
+"build/**",
+"next-env.d.ts",
+]),
 ]);
 
 export default eslintConfig;
